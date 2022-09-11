ff072f64ace986739961b034db8d36e1e3e298a2
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2022-09-11 22:57:51 +0700

    hxt

--- note/git-node.js
+++ note/git-node.js
@@ -1,3 +1,6 @@
+// local responsitory : máy mình
+// remote responsitory : sever git
+
 // source code : + hệ thống giúp lưu trữ mọi thay đổi code 
 //               + hỗ trợ nhiều người làm việc cùng lúc
 
@@ -9,8 +12,15 @@
 // 4. $ git commit -m "message" : Sau lệnh add, bạn cần sử dụng câu lệnh Commit để đây thông tin thay đổi lên Local Respository;
 // 5. $ git remote add origin <remote_url> : " nếu chưa tồn tại remote trên server thì bạn cần add mới một remote trước rồi mới push"
 // 6. $ git push origin <name_branch> : cập nhât lên server
+
 // 7. $ git branch <name_branch> : Để tạo mới một branch:
 // 8. $ git branch -b <name_branch> :Để chuyển và tạo mới:
+
+// 9. $ git pull origin master:  Lệnh git pull lấy về thông tin từ remote và cập nhật vào các nhánh của local reponsitory
+// 10.$ git fetch : được sử dụng để lấy về thông tin từ Remote repository và k cập nhập luôn vào nhánh của local reponsitory
+//  git fetch origin / master : tải thông tin (tệp) về sửa
+//  git merge origin/master : sau khi sửa gộp lại những thay đổi
+
 
 // 9. $ git checkout master:  cần phải checkout ra khỏi branch hiện tại cần gộp để vào branch master,
 // sau đó thì dùng lệnh merge để ghép branch mới vào master:
